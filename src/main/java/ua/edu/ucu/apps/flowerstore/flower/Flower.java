073e7449d6452ecae4ae6503dab2021d1a54161a
Oleh Basystyi <basystyo@gmail.com>
2024-10-28 11:17:46 +0200

    Forever

--- src/main/java/ua/edu/ucu/apps/flowerstore/flower/Flower.java
+++ src/main/java/ua/edu/ucu/apps/flowerstore/flower/Flower.java
@@ -1,11 +1,18 @@
 package ua.edu.ucu.apps.flowerstore.flower;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @Setter @Getter @ToString @AllArgsConstructor
+@Entity
+@Table
 public class Flower extends Item{
+    @Id
+    private Long id;
     private String color;
     private int sepalLength;
     private double price;
@@ -14,6 +21,16 @@ public class Flower extends Item{
         sepalLength = 0;
         price = 0;
     }
+    public double getPrice(){
+        return price;
+    }
+    public Flower(String color, int sepalLength, double price, FlowerType type){
+        this.color = color;
+        this.sepalLength = sepalLength;
+        this.price = price;
+        this.type = type;
+    }
+
     public Flower(Flower flower) {
         color = flower.color;
         sepalLength = flower.sepalLength;
